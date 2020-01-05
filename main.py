447027c835839965a26dc0d394e6043cb06941c3
Wellington Pardim <wellpardim10@gmail.com>
2020-01-04 21:28:33 -0300

    Create functions to get user input
    * Create a function to get a project path
    * Create a function to get the file extensions to modify
    * Create a .gitginore file

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-import os
+import os, sys
+# path = '/home/wellington/Documents/fretebras/fretebras-site'
+def get_files():    
 
-path = '/home/wellington/Documents/fretebras/fretebras-site'
+    project_path = get_project_path()
+    file_extensions = get_files_extensions()
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(project_path):
+        for file in f:
+            if '.php' in file:
+                files.append(os.path.join(r, file))
+    return files
 
-files = []
-# r=root, d=directories, f = files
-for r, d, f in os.walk(path):
-    for file in f:
-        if '.php' in file:
-            files.append(os.path.join(r, file))
-print(len(files))
+def get_files_extensions():
+    file_extensions = input("Enter the file extensions you want to modify separated by " \
+                             "commas:\n")
 
-# for f in files:
-#     print(f)
+    file_extensions = file_extensions.split(',')
+    print(file_extensions)
+
+def get_project_path():
+    project_path = input("Enter the absoulte project path or hit enter to use your current directory:\n")
+
+    # user entered enter then get cwd
+    if not project_path:
+        project_path = os.getcwd()
+
+    if not os.path.isdir(project_path):
+        print("Entered path doesn't exist. Aborting...")
+        sys.exit(0)
+    
+    print("Path entered: " + project_path)
+    
+    return project_path
+def main():
+    # check if it's being executed directly
+    if __name__ == "__main__":
+        get_files()
+
+main()
