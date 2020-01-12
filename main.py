8a8250e788f3840b397d2186511cce852aa9fc3e
Wellington Pardim <wellpardim10@gmail.com>
2020-01-11 22:58:53 -0300

    refactor code

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import os, sys
 # path = '/home/wellington/Documents/fretebras/fretebras-site'
-def get_files():    
+
+def modify_file():
+    pass
+
+def get_filepaths():    
 
     project_path = get_project_path()
-    file_extensions = get_files_extensions()
-    files = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(project_path):
-        for file in f:
-            if '.php' in file:
-                files.append(os.path.join(r, file))
-    return files
+    file_exts = get_files_extensions()
+
+    # array to store filepaths with matched extensions
+    filepaths = []
+    # walk over the entire project looking for the extensions
+    for root, directories, files in os.walk(project_path):
+        for file in files:
+            #  get file extension
+            file_ext = os.path.splitext(file)[-1]
+            if file_ext in file_exts:
+                filepaths.append(os.path.join(root, file))
+    return filepaths
 
 def get_files_extensions():
-    file_extensions = input("Enter the file extensions you want to modify separated by " \
+    exts = input("Enter the file extensions you want to modify separated by " \
                              "commas:\n")
+    
+    # remove trailing and leading space
+    exts = list(map(str.strip, exts.split(',')))  
+    # add dots if needed 
+    exts = [ ext if ext[0] == '.' else '.' + ext for ext in exts ]
 
-    file_extensions = file_extensions.split(',')
-    print(file_extensions)
+    return exts
 
 def get_project_path():
     project_path = input("Enter the absoulte project path or hit enter to use your current directory:\n")
 
-    # user entered enter then get cwd
+    # user entered enter, then get cwd
     if not project_path:
         project_path = os.getcwd()
 
@@ -36,6 +48,6 @@ def get_project_path():
 def main():
     # check if it's being executed directly
     if __name__ == "__main__":
-        get_files()
+        get_filepaths()
 
 main()
